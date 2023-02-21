d94abf63186050fcd6b7f9a4cb1c43d8475016ad
paludit <udit.pal@flobiz.in>
2023-02-21 18:05:43 +0530

    implemented jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
   resources :comments
+
+  resources :users, only: [:create]
+
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
 end
