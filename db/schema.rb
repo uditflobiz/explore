173ce24a9c4e7d6c6be2794db32031abc6ef39b1
paludit <udit.pal@flobiz.in>
2023-02-22 13:06:12 +0530

    added otp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_113450) do
+ActiveRecord::Schema.define(version: 2023_02_22_072947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2023_02_21_113450) do
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "otp_secret_key"
+    t.integer "otp_counter", default: 0
   end
 
   add_foreign_key "comments", "articles"
